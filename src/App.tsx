8311e25ffd7a883d3fe56073d1e01430995e895b
DAFAAHMADFAHRISI <dafamangku@gmail.com>
2025-08-05 09:26:39 +0700

    Mengatur Posisi Direktori /Guru/Tugas

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import DataAbsensi from './Page/SuperAdmin/Data Absensi';
 import DataMentorSiswa from './Page/SuperAdmin/Data Mentor-Siswa';
 import DataGuruSiswa from './Page/SuperAdmin/Data Guru - Siswa';
 import DataRekap from './Page/SuperAdmin/Data Rekap';
-import DataTugasGuru from './Page/Guru/Data Tugas';
+import DataTugasGuru from './Page/Guru/Tugas/Data Tugas';
 import DataAbsensiGuru from './Page/Guru/Data Absensi';
 import DataRekapGuru from './Page/Guru/Data Rekap';
 import TugasMentor from './Page/Mentor/TugasMentor';
